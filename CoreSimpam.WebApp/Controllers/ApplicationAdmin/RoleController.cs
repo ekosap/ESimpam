75c0a9132525e38472cd013038284c854cb58cb7
Eko Saputro <esaputro1994@gmail.com>
2021-03-20 19:57:32 +0700

    Add project files.

---
+++ CoreSimpam.WebApp/Controllers/ApplicationAdmin/RoleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreSimpam.WebApp.Controllers.ApplicationAdmin
+{
+    public class RoleController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Title = "Application Role";
+            return await Task.FromResult(View());
+        }
+        public async Task<IActionResult> Add()
+        {
+            ViewBag.Title = "Add Application Role";
+            return await Task.FromResult(PartialView("_Add"));
+        }
+    }
+}
